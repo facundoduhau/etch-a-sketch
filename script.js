351fefe4ac6ba317a1a878bc96d1e0f25058fe39
facundoduhau <duhaufacundoarg@gmail.com>
2024-01-05 02:22:47 -0300

    Added console.log that tells you what the slider is set to, and how many divs are needed for the rows and columns to be equal to the slider value

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-var gridArea;
-
 // Gets screen density value, and prints it next to the slidebar
 const slider = document.querySelector("input");
 const value = document.querySelector(".value");
 value.textContent = slider.value;
 
+//Function that gets the value of the bar at a given point, 
 function getSliderValue()
 {
     value.textContent = this.value;
-    gridArea = (slider.value*slider.value)
-    console.log(gridArea);
+    console.log("The slider has a value of " + slider.value + " in the slide bar")
 };
-
-slider.addEventListener("input", getSliderValue());
 
 // Adds "pixels" as a value from the slidebar
 function addPixel()
@@ -21,12 +17,22 @@ function addPixel()
     var pixel = document.createElement('div');
     pixel.className="pixels"
     screen.appendChild(pixel);
-}
+};
 
-// the grid Area is side * side, so, this function helps to make an even grid, by multiplying the chosen value times itself
-function sideBySide()
+// Function that multiplyes slider value times itself, to get the area of the grid
+function gridArea()
 {
-    gridArea = (slider.value*slider.value)
-    console.log(gridArea);
+    let gridArea = (slider.value*slider.value)
+    console.log("The grid has an area of " + gridArea + " pixels");
 }
 
+// When the user changes the slide bar value, it returns the new value
+slider.oninput = function()
+{
+    getSliderValue();
+    gridArea();
+};
+
+
+// Sets the grid to the default value (50), and also the grid starting value (2500)
+getSliderValue(); gridArea();
