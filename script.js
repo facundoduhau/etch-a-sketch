c1e18c2d499f31668d7d56c12281b83fc6a9ffef
facundoduhau <duhaufacundoarg@gmail.com>
2024-01-04 21:34:49 -0300

    Added function to calculate the needed divs for grid, according to input the user gives with the slide bar

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-// Gets screen density value, and prints it next to the slidebar
+var gridArea;
 
+// Gets screen density value, and prints it next to the slidebar
 const slider = document.querySelector("input");
 const value = document.querySelector(".value");
 value.textContent = slider.value;
-slider.oninput = function()
+
+function getSliderValue()
 {
     value.textContent = this.value;
-}
+    gridArea = (slider.value*slider.value)
+    console.log(gridArea);
+};
+
+slider.addEventListener("input", getSliderValue());
 
 // Adds "pixels" as a value from the slidebar
+function addPixel()
+{
+    const screen = document.querySelector('#screen');
+    var pixel = document.createElement('div');
+    pixel.className="pixels"
+    screen.appendChild(pixel);
+}
 
-const screen = document.querySelector('#screen');
-var pixel = document.createElement('div');
-screen.appendChild(pixel);
+// the grid Area is side * side, so, this function helps to make an even grid, by multiplying the chosen value times itself
+function sideBySide()
+{
+    gridArea = (slider.value*slider.value)
+    console.log(gridArea);
+}
+
