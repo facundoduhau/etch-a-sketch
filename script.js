2a606c7bc0471361e4867af1259e61b2e75fd463
facundoduhau <duhaufacundoarg@gmail.com>
2024-04-01 16:50:57 -0300

    Finished project, should add a parameter for the chosen grid size so that it only accepts values between 2 and 100

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
+// Event listener for the grid size
 var sizeButton = document.getElementById('sizeButton');
 sizeButton.addEventListener('click', makeGrid);
 
-for (let i = 0; i < 256; i++)
+// Event listener for the color
+var colorInput = document.getElementById('colorInput')
+colorInput.addEventListener('input',clicked)
+
+// Event listener for clear button
+var clear = document.getElementById('clear')
+clear.addEventListener('click',clearGrid)
+
+// Variable initialization for the starting pixel size, and brush color
+var pixelSize = 25;
+var colorValue = colorInput.value;
+var gridSize = 256
+
+// Makes starting grid
+for (let i = 0; i < gridSize; i++)
 {
     makePixel();
 }
 
+// Deletes grid
 function deleteGrid()
 {
     grid.innerHTML = '';
 }
 
+// Appends pixels to the gridholder, also gives each pixel a class and size
 function makePixel()
 {
     var grid = document.getElementById('grid')
     var pixel = document.createElement('div');
     pixel.className = "pixel";
+    pixel.style.width = pixelSize.toString() + "px";
+    pixel.style.backgroundColor = "white";
+    pixel.addEventListener("mouseover", function(e)
+    {
+        if(e.buttons === 1)
+        {
+            pixel.style.backgroundColor = colorValue;
+        }
+    })
     grid.append(pixel);
 }
 
+// Makes grid with input from the size button
 function makeGrid()
 {
-    selectedSize = prompt("What is your desired grid size?: ")
-    console.log("Selected size is:", selectedSize);
-
+    var selectedSize = prompt("What is your desired grid size?: ")
+    console.log("Selected size is:", selectedSize, "rows and columns");
     gridSize = (selectedSize * selectedSize);
     console.log("Amount of pixels needed is:", gridSize);
 
@@ -38,3 +64,17 @@ function makeGrid()
 }
 }
 
+// Updates the color value
+function clicked()
+{
+    colorValue = colorInput.value;
+}
+
+function clearGrid()
+{
+    deleteGrid();
+    for (let i = 0; i < gridSize; i++)
+    {
+        makePixel();
+    }
+}
